ac26324ba6ef1d2dfe87c56c63351d18f4c922ea
Landon Weber <Weberl8@wwu.edu>
2025-05-30 15:40:45 -0700

    skeleton

--- app/src/main/java/lab7/Huffman.java
+++ app/src/main/java/lab7/Huffman.java
@@ -11,10 +11,33 @@ public class Components {
         throw new UnsupportedOperationException();
     }
 
-    //public static buildTree()
+    public static hNode buildTree(Heap heap){}
+
+    public static Decode(hNode rootNode, String codedString){}
+
+    public static Encode(hNode rootNode, String inputString){}
+
 
     public static void main(String[] args) {
         System.out.println(new Components().getGreeting());
     }
+
+    public class hNode{
+
+        public Entry entry;
+        public hNode parent;
+        public hNode leftChild;
+        public hNode rightChild; 
+
+        public hNode(Entry e, hNode p, hNode l, hNode r){
+            e = entry;
+            p = parent;
+            l = leftchild;
+            r = rightChild;
+        }
+    
+    }
+
+    
 
 }
