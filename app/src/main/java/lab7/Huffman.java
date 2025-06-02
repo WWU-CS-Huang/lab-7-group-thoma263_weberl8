d6379917ebbd920f16404935ec0fee5cb2f4f04d
Landon Weber <Weberl8@wwu.edu>
2025-06-02 11:59:30 -0700

    updated countFrequencies

--- app/src/main/java/lab7/Huffman.java
+++ app/src/main/java/lab7/Huffman.java
@@ -1,39 +1,60 @@
+package lab7;
 import heap.Heap;
 
-package lab7;
+public class Huffman {
 
-public class Components {
+    Heap heap = new Heap();
 
-    heap = new Heap();
+    public void countFrequencies(String string){
+        for (int i = 0; i < string.length(); i++){
 
-    public static void countFrequenies(String string){
-        for (i = 0; i < string.length)
-        throw new UnsupportedOperationException();
+            //if string[i] is not in heap, add new node with string[i] v with priority 1
+            if (heap.contains(string.charAt(i))==false){
+                heap.add(string.charAt(i), 1);
+            //if string[i] is contained in heap, find string[i] node and ++ priority
+            } else {
+                heap.changePriority(string.charAt(i), heap.getPriority(string.charAt(i))+1);
+            }
+
+        }
+        
     }
 
-    public static hNode buildTree(Heap heap){}
+    public static hNode buildTree(Heap heap){
+        return;
+    }
 
-    public static Decode(hNode rootNode, String codedString){}
+    public static int Decode(hNode rootNode, String codedString){
+        return 1;
+    }
 
-    public static Encode(hNode rootNode, String inputString){}
+    public static int Encode(hNode rootNode, String inputString){
+        return 1;
+    }
 
 
     public static void main(String[] args) {
-        System.out.println(new Components().getGreeting());
+
+        String string = "kasdjlf;j;asdf";
+        countFrequencies(string);
     }
 
     public class hNode{
 
-        public Entry entry;
+        public String value;
+        public int priority;
         public hNode parent;
         public hNode leftChild;
         public hNode rightChild; 
 
-        public hNode(Entry e, hNode p, hNode l, hNode r){
-            e = entry;
-            p = parent;
-            l = leftchild;
-            r = rightChild;
+        public hNode(String v, int pri, hNode p, hNode l, hNode r){
+
+            value = v;
+            priority = pri;
+            parent = p;
+            leftChild = l;
+            rightChild = r;
+            
         }
     
     }
